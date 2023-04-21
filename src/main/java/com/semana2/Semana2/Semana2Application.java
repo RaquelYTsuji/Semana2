d45012fac20d861d9f9fa16dd0214de8b8ca3ab3
Raquel Tsuji <raqueltsuji@gmail.com>
2023-04-20 23:27:26 -0300

    Commit init

---
+++ src/main/java/com/semana2/Semana2/Semana2Application.java
@@ -0,0 +1,13 @@
+package com.semana2.Semana2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semana2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/semana2/Semana2/Semana2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semana2.Semana2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
