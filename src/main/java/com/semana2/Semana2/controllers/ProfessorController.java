41e1531e59ec7804b1194dd3acd1d35fccc5b883
Raquel Tsuji <raqueltsuji@gmail.com>
2023-04-23 16:43:33 -0300

    Adicionando detalhes

--- src/main/java/com/semana2/Semana2/controllers/ProfessorController.java
+++ src/main/java/com/semana2/Semana2/controllers/ProfessorController.java
@@ -11,9 +11,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ProfessorController {
@@ -49,13 +51,26 @@ public class ProfessorController {
         }else{
             Professor professor = requisicao.toProfessor();
             this.professorRepository.save(professor);
+            return new ModelAndView("redirect:/professores/" + professor.getId());
+        }
+    }
+
+    @GetMapping("/professores/{id}")
+    public ModelAndView show(@PathVariable Long id){
+        Optional<Professor> optional = this.professorRepository.findById(id);
+        if (optional.isPresent()){
+            Professor professor = optional.get();
+            ModelAndView mv = new ModelAndView("professores/show");
+            mv.addObject("professor", professor);
+            return mv;
+        }else{
+            System.out.println("$$$$$$$$$$ Não achou o professor de ID: " +id+ "$$$$$$$$$$$$$");
             return new ModelAndView("redirect:/professores");
         }
     }
 
     @ModelAttribute("servletPath")
     String getRequestServletPath(HttpServletRequest request) {
-        System.out.println(request.getServletPath());
         return request.getServletPath();
     }
 }
