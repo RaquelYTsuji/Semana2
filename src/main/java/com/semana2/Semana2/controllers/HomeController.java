4077621a24f2c27ebbe5893d4cab80a7f5c5e43d
Raquel Tsuji <raqueltsuji@gmail.com>
2023-04-23 15:46:02 -0300

    Atualizando fregmentos

--- src/main/java/com/semana2/Semana2/controllers/HomeController.java
+++ src/main/java/com/semana2/Semana2/controllers/HomeController.java
@@ -1,12 +1,20 @@
 package com.semana2.Semana2.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String home(){
         return "home";
+    }
+
+    @ModelAttribute("servletPath")
+    String getRequestServletPath(HttpServletRequest request) {
+        System.out.println(request.getServletPath());
+        return request.getServletPath();
     }
 }
